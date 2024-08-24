cb44407cfd8161da29f82d1c539d273cab71ad41
Swee <120145778+Sweeistaken@users.noreply.github.com>
2024-08-23 18:27:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.3' 
+VERSION = '1.0.4' 
 DESCRIPTION = 'An easy and fun encryption module.'
-LONG_DESCRIPTION = 'SweeCrypt is an easy and fun encryption module that turn letters and numbers to symbols found on the keyboard.'
+LONG_DESCRIPTION = open('README.md').read()
 
 setup(
         name="sweecrypt", 
         version=VERSION,
         author="SweeZero",
         author_email="swee@mailfence.com",
-        readme = "README.md",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
